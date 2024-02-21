1b87f1c0fe2e4c8669726c0537eeeab79cd91a32
szy her <szymonhernik123@gmail.com>
2024-02-21 16:18:01 +0100

    gallery fix rounded width and height for image (sanity not accepting urls like this) and different image sizes for vertical and horizontal

--- components/Galleries.tsx
+++ components/Galleries.tsx
@@ -26,16 +26,27 @@ export async function Galleries({ galleries }) {
                 className="mySwiper h-[50vh] flex flex-col items-center justify-center !overflow-x-scroll"
               >
                 {gallery.images.map((image, index) => {
+                  // Define sizes based on aspect ratio
+                  let sizes
+                  if (image.asset.aspectRatio > 1) {
+                    // Landscape (horizontal) image
+                    sizes =
+                      '(max-width:640px) 100vw, (max-width: 768px) 50vw, 50vw'
+                  } else {
+                    // Portrait (vertical) or square image
+                    sizes =
+                      '(max-width:640px) 100vw, (max-width: 768px) 75vw, 30vw'
+                  }
                   return (
                     <SwiperSlide
                       className="!w-fit !mr-8"
                       key={gallery._key + image + index}
                     >
                       <Image
-                        src={`${image.asset.url}?w=${image.asset.width / 2}&h=${image.asset.height / 2}`}
-                        width={image.asset.width}
-                        height={image.asset.height}
-                        sizes="(max-width:640px) 100vw, (max-width: 768px) 50vw, 30vw"
+                        src={`${image.asset.url}?w=${Math.round(image.asset.width / 2)}&h=${Math.round(image.asset.height / 2)}`}
+                        width={Math.round(image.asset.width)}
+                        height={Math.round(image.asset.height)}
+                        sizes={sizes}
                         alt={`${image?.alt ?? ''}`}
                         className="h-full min-w-fit w-fit"
                         blurDataURL={image.asset.lqip}
